800cdd19d4ae6ec7931e8d2a23fd3abee5caa143
rajkumaraddala <115741752+rajkumaraddala@users.noreply.github.com>
2022-11-06 19:53:26 +0530

    Add files via upload

---
+++ 1,2 app.js
@@ -0,0 +1,64 @@
+console.log( 'My Name is Rajkumar!! my age is 25');
+ console.log('I have been programming form past 3 weeks');
+ console.log('Catch-22 by Joseph Heller');
+ console.log('It was love at first sight.');//opening line//
+
+console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');/* here whole data ends*/
+console.log('But this just being short of jaundice all the time confused them.');
+
+/*qust 3 starts here*/
+const name="java script";
+const year='2011';
+const value='20.49';
+ console.log(name);
+ console.log(year);
+ console.log("Woohoo! I love to code! #codecademy");
+ console.log( value);
+
+//task 4//
+
+console.log( '3.5 This is the age  when we start sending people to live on Mars');
+
+ console.log(2022-1969);
+ console.log(240/65);
+ console.log(0.2708 %100.);
+
+
+  //task5//
+
+
+ console.log("helloworld");
+ console.log("hello"+" "+"world");
+
+ // task6//
+
+ let string ="Teaching the world how to code";
+ console.log(string.length);
+
+
+ //task7//
+
+ let  text = "Codecademy";
+ console.log(text.toUpperCase());
+
+let name3='Remove whitespace';
+ console.log(name3.trim());
+
+
+  //task8//
+
+  
+  let randomnumber=Math.floor(Math.random()*100);
+  console.log(randomnumber);
+  console.log(Math.floor(43.8));
+  console.log(Number.isInteger(2017));
+  
+
+ 
+
+
+
